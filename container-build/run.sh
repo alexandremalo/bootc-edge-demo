9a30a63d1459d7f3cf9b92690ea1ffb1b2b08e97
alexandremalo <alexandre@malo.tech>
2025-05-27 09:18:08 -0400

    changing to baser message

--- container-build/run.sh
+++ container-build/run.sh
@@ -1,2 +1,2 @@
-podman build . -t quay.io/alexmalo/bootc/demo:2
-podman push quay.io/alexmalo/bootc/demo:2
+podman build . -t quay.io/alexmalo/bootc/demo:3
+podman push quay.io/alexmalo/bootc/demo:3
